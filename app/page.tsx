6af88becf8f32643bbe0c9ad3d2bd1054de8fc07
Iulian Carnaru <iulian.carnaru@gmail.com>
2025-07-18 09:25:41 +0100

    add shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <h1>Hi</h1>;
+  return (
+    <div>
+      <h1>job board</h1>
+      <Button>Click me</Button>
+    </div>
+  );
 }
