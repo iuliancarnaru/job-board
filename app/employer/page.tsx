29fd366dfd4ad1fade85d2cee36a6258f8459860
Iulian Carnaru <iulian.carnaru@gmail.com>
2025-12-19 07:54:27 +0000

    display listing and edit listing

--- app/employer/page.tsx
+++ app/employer/page.tsx
@@ -1,7 +1,7 @@
 import { db } from "@/drizzle/db";
 import { JobListingTable } from "@/drizzle/schema";
-import { getJobListingsOrganizationTag } from "@/features/jobListings/db/cache/jobListings";
 import { getCurrentOrganization } from "@/services/clerk/lib/getCurrentAuth";
+import { getJobListingOrganizationTag } from "@/features/jobListings/db/cache/jobListings";
 import { desc, eq } from "drizzle-orm";
 import { cacheTag } from "next/dist/server/use-cache/cache-tag";
 import { redirect } from "next/navigation";
@@ -33,8 +33,7 @@ async function SuspendedPage() {
 
 async function getMostRecentJobListing(orgId: string) {
   "use cache";
-
-  cacheTag(getJobListingsOrganizationTag(orgId));
+  cacheTag(getJobListingOrganizationTag(orgId));
 
   return db.query.JobListingTable.findFirst({
     where: eq(JobListingTable.organizationId, orgId),
